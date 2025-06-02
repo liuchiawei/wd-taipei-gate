a0e586dccd1e1f2b23ef63e5ce8447e9649f0b68
LIU CHIAWEI <doublecheap@gmail.com>
2025-06-02 22:28:34 +0900

    feat/photo: add flip card

--- src/components/common/mainItem.tsx
+++ src/components/common/mainItem.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useState } from "react"
 import Image from "next/image"
 import { motion } from 'motion/react'
+import ReactCardFlip from "react-card-flip" 
 import { Gate } from "@/lib/types/type"
 import Avatar from "@/components/common/avatar"
 import SplitText from "@/components/common/splitText"
@@ -15,6 +17,14 @@ export default function MainItem({ data }: { data: Gate }) {
       y: 0,
       transition: { type: "spring", bounce: 0.6, duration: i * 0.1 + 0.2 } }),
   }
+    const [isFlipped, setIsFlipped] = useState(false);
+  /** 
+    @function handleFlip
+    @description: カードを裏返す関数
+  */
+  const handleFlip = () => {
+    setIsFlipped((prev) => !prev);
+  };
 
   return (
     <div 
@@ -22,27 +32,32 @@ export default function MainItem({ data }: { data: Gate }) {
       className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8 w-full h-full p-8 mx-auto scroll-mt-24"
     >
       {/* 画像 */}
-      <div className="row-span-3 flex flex-col items-center justify-center relative w-full h-full p-12 bg-neutral-200 dark:bg-neutral-700 rounded-xl">
-        <motion.div
-          className="flex flex-col items-center justify-center"
-          initial={{ opacity: 0, y: 120, scale: 0.9 }}
-          whileInView={{ opacity: 1, y: 0, scale: 1 }}
-          transition={{ type: "spring", bounce: 0.6, stiffness: 200, duration: 0.2, delay: 0.2 }}
-        >
-          <Image src={data.image} alt={data.name} width={300} height={300} />
-        </motion.div>
-        <motion.div
-          className="border-b border-2 border-stone-700 dark:border-neutral-900 z-10 origin-bottom-left"
-          initial={{ width: "0%" }}
-          whileInView={{ width: "100%" }}
-          transition={{ type: "spring", duration: 0.3 }}
-        />
-        {/* 画像タイトル */}
-        <SplitText
-          text={data.name}
-          className="text-5xl font-title my-4 tracking-widest"
-        />
-      </div>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal" containerClassName="row-span-3">
+        <div className="flex flex-col items-center justify-center relative w-full h-full p-12 bg-neutral-200 dark:bg-neutral-700 rounded-xl" onClick={handleFlip}>
+          <motion.div
+            className="flex flex-col items-center justify-center"
+            initial={{ opacity: 0, y: 120, scale: 0.9 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ type: "spring", bounce: 0.6, stiffness: 200, duration: 0.2, delay: 0.2 }}
+          >
+            <Image src={data.image} alt={data.name} width={300} height={300} />
+          </motion.div>
+          <motion.div
+            className="border-b border-2 border-stone-700 dark:border-neutral-900 z-10 origin-bottom-left"
+            initial={{ width: "0%" }}
+            whileInView={{ width: "100%" }}
+            transition={{ type: "spring", duration: 0.3 }}
+          />
+          {/* 画像タイトル */}
+          <SplitText
+            text={data.name}
+            className="text-5xl font-title my-4 tracking-widest"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center w-full h-full rounded-xl overflow-hidden bg-cover bg-center" style={{ backgroundImage: `url(${data.photo})` }} onClick={handleFlip} />
+
+      </ReactCardFlip>
+
       {/* タイトル */}
       <div className="hidden md:block md:[writing-mode:vertical-rl] relative h-fit">
         <SplitText
